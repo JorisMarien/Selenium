f6033dd55cbef1dfff7cf7cb747e80551b8d152f
m-joris@hotmail.be <m-joris@hotmail.be>
2023-06-02 09:23:45 +0200

    refactored to test directory

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,8 @@
 package org.example;
 
-import org.example.Login;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Login login = new Login();
-        login.test();
     }
 
 
--- src/test/java/org/test/Login.java
+++ src/test/java/org/test/Login.java
@@ -1,5 +1,6 @@
-package org.example;
+package org.test;
 
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,16 +8,21 @@ import org.openqa.selenium.WebElement;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Login {
-    public void test(){
+    @Test
+    public void loginTest(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/login");
+
+        String username = "tomsmith";
+        String password = "SuperSecretPassword!";
 
         WebElement textBoxUsername = driver.findElement(By.id("username"));
         WebElement textBoxPassword = driver.findElement(By.id("password"));
         WebElement loginButton = driver.findElement(By.className("fa-sign-in"));
 
-        textBoxUsername.sendKeys("tomsmith");
-        textBoxPassword.sendKeys("SuperSecretPassword!");
+        textBoxUsername.sendKeys(username);
+        textBoxPassword.sendKeys(password);
+
         loginButton.click();
 
         WebElement greenBannerLoginSuccess = driver.findElement(By.id("flash"));
@@ -24,10 +30,10 @@ public class Login {
         loginSuccessText = loginSuccessText.substring(0,loginSuccessText.length()-2);
         assertEquals("You logged into a secure area!", loginSuccessText);
 
+
         WebElement logoutButton = driver.findElement(By.className("icon-signout"));
         logoutButton.click();
 
         driver.quit();
     }
-
 }
